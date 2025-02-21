1f043e6a55b91a19d5d95e11a6b81f7c66b675d7
Geraldo JR <geraldo.sobreira.junior@ccc.ufcg.edu.br>
2025-02-21 14:25:47 -0300

    Switching to k8s-api from submodules

--- README.md
+++ README.md
@@ -45,12 +45,12 @@ To create your emulated cluster, you first need **access to AWS** and must insta
 | `CALICO_NAMESPACE`      | Namespace to install Calico |
 
 Once you have these dependencies installed, copy the `env.example` file to your `.env` using:
-```
+```bash
 cp .env.example .env
 ```
 
 After this, init the submodule repositories `prometheus` and `karpenter-code` using
-```
+```bash
 git submodule update --init --recursive
 ```
 ---
@@ -62,27 +62,31 @@ Please visit the `README.md` in `trace-emulator/data` to understand how your fil
 ### 🔑 Grant Execution Permission  
 Before running an emulation, grant execution permission to the **execute_emulation.sh** file by running:
 
-```
+```bash
 chmod +x execute_emulation.sh
 ```
-### ▶️ Run the Execution Manager  
+### ▶️ Running the Execution Manager  
 There are multiple ways to run our emulation tool. One method is by using an **existing cluster** with at least **one node**:
 
-```
+```bash
 ./execute_emulation.sh --sim --use-cluster <cluster-context> --trace-path <trace-path>
 ```
 You can also create a **new cluster** with **one node**. To do that, you must have the roles set in your .env file:
 
-```
+```bash
 ./execute_emulation.sh --sim --new-cluster --trace-path <trace-path>
 ```
-If you don't have sure about the flags, please use the following command:
-```
+If you are unsure about the flags, please use the following command:
+```bash
 ./execute_emulation.sh --help
 ```
+
+> **Note:** If you want to use our solution to generate the input files for the infrastructure and workload manager, please read the [README here](https://github.com/ufcg-lsd/klue/tree/main/trace-emulation/data#readme).
+
 ---
-## Exemplo de execução da ferramenta
+## 🧪 Example of Tool Execution  
 ![Demo do KLUE](assets/emulation-running.gif)
+
 ---
 
 ## 👥 Developed by VTEX Lab Members  
--- trace-emulation/pods_mapping.py
+++ trace-emulation/pods_mapping.py
@@ -1,29 +1,29 @@
 import pandas as pd
-from util.k8s_api.k8s_client import K8SClient
+from util.k8s_api.k8s_api import K8SAPI
 import re
 
 PODS_ALLOCATION_PATH = "/tmp/pods_allocation.csv"
 
 pods_allocation = pd.read_csv(PODS_ALLOCATION_PATH)
 
-k8s_client = K8SClient()
+k8s_api = K8SAPI()
 
-def get_fake_nodes_dict(k8s_client: K8SClient):
+def get_fake_nodes_dict(k8s_api: K8SAPI):
     print("\nFetching cluster nodes with node pool and instance type:")
 
     node_data = {}
 
-    nodes = k8s_client.list_node()
+    nodes = k8s_api.list_node()
     real_node_regex = r"^ip-\d{3}-\d{2}-\d{2}-\d{3}\..*"
-    
+
     for node in nodes.items:
         node_name = node.metadata.name
         node_pool = node.metadata.labels.get('karpenter.sh/nodepool')
         instance_type = node.metadata.labels.get('node.kubernetes.io/instance-type')
-        
+
         if instance_type not in node_data:
             node_data[instance_type] = []
-        
+
         if not re.match(real_node_regex, node_name):
             node_data[instance_type].append([node_name, node_pool])
 
@@ -42,29 +42,29 @@ def get_real_nodes_dict(pods_allocation: pd.DataFrame):
         if instance_type not in node_data:
             node_data[instance_type] = []
         node_data[instance_type].append([node_name, node_pool])
-        
+
     return node_data
 
 
-def get_owners_with_pods(k8s_client: K8SClient):
+def get_owners_with_pods(k8s_api: K8SAPI):
     owners_with_pods = {}
 
-    pods = k8s_client.list_pod_for_all_namespaces()
+    pods = k8s_api.list_pod_for_all_namespaces()
 
     for pod in pods.items:
-        owner_name = pod.metadata.labels.get('deployment')  
+        owner_name = pod.metadata.labels.get('deployment')
 
         if owner_name:
             if owner_name not in owners_with_pods:
-                owners_with_pods[owner_name] = []  
-            owners_with_pods[owner_name].append(pod.metadata.name) 
+                owners_with_pods[owner_name] = []
+            owners_with_pods[owner_name].append(pod.metadata.name)
 
     return owners_with_pods
 
 def map_fake_and_real_nodes(fake_nodes_dict, real_nodes_dict, pods_allocation):
     for real_instance_type, real_nodes in real_nodes_dict.items():
         if real_instance_type not in fake_nodes_dict:
-            print(f"Instância {real_instance_type} não foi encontrada no cluster real")
+            print(f"Instância {real_instance_type} não foi encontrada no cluster emulado")
             exit()
 
         for real_node in real_nodes:
@@ -103,13 +103,13 @@ def map_pods_and_nodes(pods_allocation, replicaset_pod_dict):
     result_df.to_csv("/tmp/pods_and_nodes_map.csv", index=False)
     return result_df
 
-    
-fake_nodes_dict = get_fake_nodes_dict(k8s_client)
+
+fake_nodes_dict = get_fake_nodes_dict(k8s_api)
 real_nodes_dict = get_real_nodes_dict(pods_allocation)
 
 map_fake_and_real_nodes(fake_nodes_dict, real_nodes_dict, pods_allocation)
 
-pod_owners = get_owners_with_pods(k8s_client)
+pod_owners = get_owners_with_pods(k8s_api)
 
 print(pod_owners)
 
