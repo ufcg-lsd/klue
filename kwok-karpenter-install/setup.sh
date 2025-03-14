e7cf0b3c4384001c4beb658d94cdbd51992f438c
Geraldo JR <geraldo.sobreira.junior@ccc.ufcg.edu.br>
2025-03-14 17:13:28 -0300

    removing unused files

--- kwok-karpenter-install/setup.sh
+++ kwok-karpenter-install/setup.sh
@@ -6,13 +6,6 @@ source ~/.bashrc
 
 helm install prometheus prometheus-community/kube-prometheus-stack -f values.yaml --namespace kube-system
 
-#kubectl apply --server-side -f kube-prometheus/manifests/setup
-#kubectl wait \
-#	--for condition=Established \
-#	--all CustomResourceDefinition \
-#	--namespace=monitoring
-#kubectl apply -f kube-prometheus/manifests/
-
 cd karpenter-code
 
 make toolchain
@@ -26,15 +19,8 @@ kubectl get po -A
 cd ..
 
 kubectl apply -f configuration-files/karpenter-servicemonitor.yml
-#./install-kwok.sh
 
 while [[ $(kubectl get pod prometheus-prometheus-kube-prometheus-prometheus-0 -n kube-system -o jsonpath='{.status.phase}') != "Running" ]]; do
   sleep 5
 done
 
-# ID 6417
-# Access Prometheus
-# kubectl port-forward --address 0.0.0.0 pod/prometheus-k8s-0  30222:9090 -n monitoring &
-# Access Grafana
-# kubectl port-forward --address 0.0.0.0 pod/my-prometheus-grafana-{hash}  3000:3000 -n monitoring &
-# kubectl get secret my-prometheus-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 --decode; echo
