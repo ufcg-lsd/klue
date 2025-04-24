502a16212f30c82737e0db49370e4b28c80d6d05
KaykyFidelis <kayky.fidelis.serafim@ccc.ufcg.edu.br>
2025-04-24 11:07:59 -0300

    build: updating the build files

--- kwok-karpenter-install/setup.sh
+++ kwok-karpenter-install/setup.sh
@@ -4,7 +4,14 @@ source ~/.bashrc
 
 # Setup Prometheus and Grafana
 
-helm install prometheus prometheus-community/kube-prometheus-stack -f values.yaml --namespace kube-system
+kubectl create namespace monitoring
+
+kubectl apply --server-side -f kube-prometheus/manifests/setup
+kubectl wait \
+	--for condition=Established \
+	--all CustomResourceDefinition \
+	--namespace=monitoring
+kubectl apply -f kube-prometheus/manifests/
 
 cd karpenter-code
 
@@ -19,8 +26,15 @@ kubectl get po -A
 cd ..
 
 kubectl apply -f configuration-files/karpenter-servicemonitor.yml
+./install-kwok.sh
 
-while [[ $(kubectl get pod prometheus-prometheus-kube-prometheus-prometheus-0 -n kube-system -o jsonpath='{.status.phase}') != "Running" ]]; do
+while [[ $(kubectl get pod prometheus-k8s-0 -n monitoring -o jsonpath='{.status.phase}') != "Running" ]]; do
   sleep 5
 done
 
+# Access Prometheus
+# kubectl port-forward --address 0.0.0.0 pod/prometheus-k8s-0  30222:9090 -n monitoring &
+
+# Access Grafana
+# kubectl port-forward --address 0.0.0.0 pod/my-prometheus-grafana-{hash}  3000:3000 -n monitoring &
+# kubectl get secret my-prometheus-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 --decode; echo
