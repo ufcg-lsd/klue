6f1e27e700aeb0e4ab0359c9199304b7e9794f0a
Mapalmeira <matheus.palmeira.leite@gmail.com>
2026-03-23 13:53:56 -0300

    fix: main now passes the new metric files as parameters

--- src/main.py
+++ src/main.py
@@ -45,8 +45,7 @@ class Main:
             tracer = TracerKWOKOnly(
                 os.path.join(self.trace_path, "kube_pod_container_resource_requests.csv"),
                 os.path.join(self.trace_path, "container_cpu_usage_seconds_total.csv"),
-                os.path.join(self.trace_path, "sum_container_cpu_usage_seconds_total.csv"),
-                os.path.join(self.trace_path, "sum_container_memory_working_set_bytes.csv"),
+                os.path.join(self.trace_path, "container_memory_usage_bytes.csv"),
                 os.path.join(self.trace_path, "kube_pod_owner.csv"),
                 os.path.join(self.trace_path, "kube_pod_status_phase.csv"),
                 os.path.join(self.trace_path, "kube_replicaset_owner.csv"),
