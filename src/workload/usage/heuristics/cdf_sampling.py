ceb37a4fe3d4996323bdde36c5021a4f4458aff2
Mapalmeira <matheus.palmeira.leite@gmail.com>
2026-05-04 17:17:07 -0300

    feat: adding placeholder for the expansion heuristic just for testing

--- src/workload/usage/heuristics/cdf_sampling.py
+++ src/workload/usage/heuristics/cdf_sampling.py
@@ -4,7 +4,29 @@ class CDFSamplingHeuristic:
         real_pods_usage,
         emulated_pods, 
         mapping,
-        remaining_pods,
+        remaining_emulated_pods,
         resource
     ):
-        return
+        
+        # just a placeholder for now
+        assignment = self._apply_mapping(
+            mapping=mapping,
+            real_pods_usage=real_pods_usage,
+            resource=resource
+        )
+
+        return assignment
+    
+
+    def _apply_mapping(self, mapping, real_pods_usage, resource, remaining_emulated_pods=None):
+        resource_assignment = {}
+
+        for real_pod, emulated_pod in mapping.items():
+            usage = real_pods_usage[real_pod][resource]
+            resource_assignment[emulated_pod] = usage
+        
+        if remaining_emulated_pods:
+            for emulated_pod in remaining_emulated_pods:
+                resource_assignment[emulated_pod] = 0
+
+        return resource_assignment
