badf82c2f2abb1e729b369214df5ffab21ae4161
Mapalmeira <matheus.palmeira.leite@gmail.com>
2026-05-04 16:33:20 -0300

    feat: updating heuristics interface

--- src/workload/usage/heuristics/cdf_sampling.py
+++ src/workload/usage/heuristics/cdf_sampling.py
@@ -2,10 +2,8 @@ class CDFSamplingHeuristic:
     def generate_assignment(
         self,
         real_pods_usage,
-        real_pods_limit, 
         emulated_pods, 
         mapping,
-        previous_assignment,
         remaining_pods,
         resource
     ):
--- src/workload/usage/heuristics/homogeneous_spread.py
+++ src/workload/usage/heuristics/homogeneous_spread.py
@@ -5,7 +5,6 @@ class HomogeneousSpreadHeuristic:
         real_pods_limit, 
         emulated_pods, 
         mapping,
-        previous_assignment,
         remaining_pods,
         resource
     ):
