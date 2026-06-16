318b268f8de37c55d950f45a88ec93441913b083
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2026-06-16 11:32:16 -0300

    fix: build-scheduler.sh may hang forever waiting for an exact Pending-pods count that may never be reached - Closes #13

--- src/build-scheduler.sh
+++ src/build-scheduler.sh
@@ -29,16 +29,32 @@ SCHEDULER_PID=$!
 
 echo "[SCHEDULER] [INFO] Scheduler started with PID: $SCHEDULER_PID"
 
-while true; do
-    CURRENT_PENDING_PODS=$(kubectl get pods --all-namespaces --field-selector=status.phase=Pending --no-headers | wc -l)
+# for each pod in pods_and_nodes_map, check if it's scheduled
+all_initial_pods_scheduled () {
+    while IFS=',' read -r pod namespace	node
+    do
+      # skip empty lines and headers
+      [[ -z "$pod" || -z "$namespace" ]] && continue
+      [[ "$pod" == "pod" && "$namespace" == "namespace" ]] && continue  
+    
+      node_name=$(kubectl -n "$namespace" get pod "$pod" -o jsonpath='{.spec.nodeName}')
+      if [[ -z "$node_name" ]]; then
+          echo "[SCHEDULER] [WARN] $pod has not been scheduled yet."
+          return 1
+      fi
+    done < "$PODS_AND_NODES_MAP"
+    
+    return 0
+}
 
-    if [ "$CURRENT_PENDING_PODS" -eq "$DESIRED_PENDING_PODS" ]; then
+while true; do
+    if all_initial_pods_scheduled; then
         echo "[SCHEDULER] [INFO] All initial pending pods have been scheduled. Killing scheduler."
-        kill $SCHEDULER_PID
+        kill "$SCHEDULER_PID"
         break
     fi
 
-    echo "[SCHEDULER] [INFO] There are still $CURRENT_PENDING_PODS pending pods. Expected $DESIRED_PENDING_PODS pending pods. Checking again in 30 seconds..."
+    echo "[SCHEDULER] [INFO] Not all initial pods were scheduled. Checking again in 10 seconds..."
     sleep 10
 done
 
